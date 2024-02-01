cc354d2ea9eb3abe35faa70aa28d66f869a75229
ee <e2gamedev@outlook.com>
2024-02-01 18:23:47 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # SoloJSX
 SoloJSX
+
+## Features
+- JSX that returns nodes
+- Supports attributes & events
+- No virtual DOM, no reactivity
+
+## Installation
+
+#### Using npm
+```bash
+npm i -D solojsx
+```
+
+#### Using bun
+```bash
+bun i -D solojsx
+```
+
+## Usage
+#### TypeScript example
+[SoloJSX-template-ts](https://github.com/eekelof/SoloJSX-template-ts)
+
+#### JavaScript example
+[SoloJSX-template-js](https://github.com/eekelof/SoloJSX-template-js)
+
+### Code Example
+```jsx  
+import solojsx from 'solojsx';
+
+const element = (
+  <div>
+    <h1>SoloJSX</h1>
+    <p>SoloJSX is a library that returns nodes from JSX</p>
+  </div>
+);
+document.body.appendChild(element);
+```
+
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            [elemName: string]: any;
+        }
+    }
+}
+export default function solojsx(type: string, props: { [key: string]: any } = {}, ...children: (string | HTMLElement)[]): HTMLElement {
+    const e = document.createElement(type);
+    Object.keys(props).forEach((key) => {
+        if (key.startsWith('on') && typeof props[key] === 'function')
+            e.addEventListener(key.toLowerCase().substring(2), props[key]);
+        else
+            e.setAttribute(key, props[key]);
+    });
+    children.forEach((child) => {
+        if (typeof child === 'string')
+            e.appendChild(document.createTextNode(child));
+        else if (child instanceof HTMLElement)
+            e.appendChild(child);
+    });
+    return e;
+}
