4fa3352446ae35bdaa10872c9a309ee3a002e40c
ee <e2gamedev@outlook.com>
2024-02-21 05:00:24 +0100

    fixed bug with null not triggering param assignment + bump to 1.0.4

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,16 @@ declare global {
         }
     }
 }
-export default function solojsx(type: string, props: { [key: string]: any } = {}, ...children: (string | HTMLElement)[]): HTMLElement {
+export default function solojsx(type: string, props: { [key: string]: any }, ...children: (string | HTMLElement)[]): HTMLElement {
     const e = document.createElement(type);
-    Object.keys(props).forEach((key) => {
-        if (key.startsWith('on') && typeof props[key] === 'function')
-            e.addEventListener(key.toLowerCase().substring(2), props[key]);
-        else
-            e.setAttribute(key, props[key]);
-    });
+    if (props) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function')
+                e.addEventListener(key.toLowerCase().substring(2), props[key]);
+            else
+                e.setAttribute(key, props[key]);
+        });
+    }
     children.forEach((child) => {
         if (typeof child === 'string')
             e.appendChild(document.createTextNode(child));
