84ef0fc2967bc5f624e68220dfd242067589e796
eekelof <erik.ekelof1@gmail.com>
2024-09-17 10:14:53 +0200

    optimized bundle size

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,5 @@
-let svgs = new Set(['svg', 'circle', 'ellipse', 'line', 'path', 'polygon', 'polyline', 'rect', 'g', 'defs', 'linearGradient', 'radialGradient', 'stop', 'use', 'symbol', 'text', 'tspan', 'tref', 'textPath', 'altGlyph', 'altGlyphDef', 'altGlyphItem', 'glyph', 'glyphRef', 'marker', 'metadata', 'missing-glyph', 'pattern', 'switch', 'foreignObject', 'desc', 'title'])
 export default function jsx(type, props, ...children) {
-    let e = svgs.has(type) ? document.createElementNS('http://www.w3.org/2000/svg', type) : document.createElement(type)
+    let e = ['svg', 'path'].includes(type) ? document.createElementNS('http://www.w3.org/2000/svg', type) : document.createElement(type)
     for (let [k, v] of Object.entries((props || {}))) {
         if (k.startsWith('on') && typeof v === 'function')
             e.addEventListener(k.toLowerCase().substring(2), v)
@@ -9,7 +8,6 @@ export default function jsx(type, props, ...children) {
         else
             e.setAttribute(k, v)
     }
-    let ac = c => Array.isArray(c) ? c.forEach(d => ac(d)) : c != null && e.append(c)
-    ac(children)
+    e.append(...children.flat().map(c => c != null ? c : []))
     return e
 }
