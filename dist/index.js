0de5e340670cce929e5a078aefe4ccafccdb3eb0
eekelof <erik.ekelof1@gmail.com>
2024-09-17 09:53:01 +0200

    optimized bundle size

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
-const svgs = new Set(['svg', 'circle', 'ellipse', 'line', 'path', 'polygon', 'polyline', 'rect', 'g', 'defs', 'linearGradient', 'radialGradient', 'stop', 'use', 'symbol', 'text', 'tspan', 'tref', 'textPath', 'altGlyph', 'altGlyphDef', 'altGlyphItem', 'glyph', 'glyphRef', 'marker', 'metadata', 'missing-glyph', 'pattern', 'switch', 'foreignObject', 'desc', 'title'])
+let svgs = new Set(['svg', 'circle', 'ellipse', 'line', 'path', 'polygon', 'polyline', 'rect', 'g', 'defs', 'linearGradient', 'radialGradient', 'stop', 'use', 'symbol', 'text', 'tspan', 'tref', 'textPath', 'altGlyph', 'altGlyphDef', 'altGlyphItem', 'glyph', 'glyphRef', 'marker', 'metadata', 'missing-glyph', 'pattern', 'switch', 'foreignObject', 'desc', 'title'])
 export default function jsx(type, props, ...children) {
-    const e = svgs.has(type) ? document.createElementNS('http://www.w3.org/2000/svg', type) : document.createElement(type)
-    for (const [k, v] of Object.entries((props || {}))) {
+    let e = svgs.has(type) ? document.createElementNS('http://www.w3.org/2000/svg', type) : document.createElement(type)
+    for (let [k, v] of Object.entries((props || {}))) {
         if (k.startsWith('on') && typeof v === 'function')
             e.addEventListener(k.toLowerCase().substring(2), v)
         else if (k == 'style' && typeof v === 'object')
@@ -9,6 +9,7 @@ export default function jsx(type, props, ...children) {
         else
             e.setAttribute(k, v)
     }
-    e.append(...children)
+    let ac = c => Array.isArray(c) ? c.forEach(d => ac(d)) : c != null && e.append(c)
+    ac(children)
     return e
 }
